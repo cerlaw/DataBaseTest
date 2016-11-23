f5c8ccc09db11b9ddd339b4f9775939862954f08
bossllcwn <bossllcwn@gmail.com>
2016-11-23 15:18:35 +0800

    实现跨程序分享数据

--- app/src/main/java/com/example/dell/databasetest/MyDatabaseHelper.java
+++ app/src/main/java/com/example/dell/databasetest/MyDatabaseHelper.java
@@ -3,7 +3,6 @@ package com.example.dell.databasetest;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.widget.Toast;
 
 /**
  * Created by DELL on 2016/11/14.
@@ -34,7 +33,6 @@ public class MyDatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_BOOK);
         db.execSQL(CREATE_CATEGORY);
-        Toast.makeText(mContext,"Create succeeded!",Toast.LENGTH_SHORT).show();
     }
 
     @Override
